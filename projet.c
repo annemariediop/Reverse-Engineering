5992806cf74efbdd4eab1e32e589992ca04ecd27
annie <36376853+annemariediop@users.noreply.github.com>
2018-12-29 23:59:06 +0100

    update projet.c

--- projet.c
+++ projet.c
@@ -5,9 +5,9 @@
  
 int main() {
     xmlDocPtr doc;
-    //xmlNodePtr racine;
+   
 
-    // Ouverture du fichier XML
+    // Ouverture du fichier XML et verification validité
     doc = xmlParseFile("fic1.xml");
     if (doc == NULL) {
         fprintf(stderr, "Document XML invalide\n");
@@ -16,18 +16,5 @@ int main() {
     else{
         fprintf(stderr, "Document XML valide\n");
     }
-/*
-    // Récupération de la racine
-    racine = xmlDocGetRootElement(doc);
-    if (racine == NULL) {
-        fprintf(stderr, "Document XML vierge\n");
-        xmlFreeDoc(doc);
-        return EXIT_FAILURE;
-    }
-    printf("La racine du document est : %s\n", racine->name);
-    // Libération de la mémoire
-    xmlFreeDoc(doc);
 
-    return EXIT_SUCCESS;
-*/
 }
